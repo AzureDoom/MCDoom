dbc9e0aeed9b49006bbf7713c7d2a5336642c6f2
AzureZhen <rcraft@cyberconnects2.com>
2021-10-04 00:37:07 -0400

    Marauder Teleports Randomly Less

--- src/main/java/mod/azure/doom/entity/tiersuperheavy/MarauderEntity.java
+++ src/main/java/mod/azure/doom/entity/tiersuperheavy/MarauderEntity.java
@@ -268,7 +268,6 @@ public class MarauderEntity extends DemonEntity implements IAnimatable {
 			if (f > 0.5F && this.level.canSeeSky(this.blockPosition())
 					&& this.random.nextFloat() * 30.0F < (f - 0.4F) * 2.0F) {
 				this.setTarget((LivingEntity) null);
-				this.teleport();
 			}
 		}
 
