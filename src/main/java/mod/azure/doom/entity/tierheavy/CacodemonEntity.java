70b61f69dd607bfbf1227db414c960b38fcb9124
AzureZhen <rcraft@cyberconnects2.com>
2021-10-18 22:31:06 -0400

    v4.0.14

--- src/main/java/mod/azure/doom/entity/tierheavy/CacodemonEntity.java
+++ src/main/java/mod/azure/doom/entity/tierheavy/CacodemonEntity.java
@@ -182,7 +182,7 @@ public class CacodemonEntity extends DemonEntity implements Monster, IAnimatable
 		this.goalSelector.add(7, new CacodemonEntity.LookAtTargetGoal(this));
 		this.goalSelector.add(4, new RangedStaticAttackGoal(this,
 				new FireballAttack(this, true).setDamage(10).setProjectileOriginOffset(1.5, 0.3, 1.5).setSound(
-						ModSoundEvents.CACODEMON_FIREBALL, 1.0F, 1.2F / (this.getRandom().nextFloat() * 0.2F + 0.9F)),
+						ModSoundEvents.CACODEMON_AFFECTIONATE_SCREAM, 1.0F, 1.2F / (this.getRandom().nextFloat() * 0.2F + 0.9F)),
 				60, 20, 30F, 1));
 		this.goalSelector.add(5, new RandomFlyConvergeOnTargetGoal(this, 2, 15, 0.5));
 		this.targetSelector.add(1, new ActiveTargetGoal<>(this, PlayerEntity.class, 10, true, false, (p_213812_1_) -> {
