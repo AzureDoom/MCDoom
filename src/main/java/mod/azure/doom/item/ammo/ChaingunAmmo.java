8d6fa3b94191daa311486c378e614d41c665e55e
AzureZhen <rcraft@cyberconnects2.com>
2021-11-06 14:05:53 -0400

    Add missing lang files

--- src/main/java/mod/azure/doom/item/ammo/ChaingunAmmo.java
+++ src/main/java/mod/azure/doom/item/ammo/ChaingunAmmo.java
@@ -11,6 +11,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 
 public class ChaingunAmmo extends ArrowItem {
@@ -24,7 +25,7 @@ public class ChaingunAmmo extends ArrowItem {
 
 	@Override
 	public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
-		tooltip.add(new TranslatableText("\u00A7o" + "Used for the Chaingun."));
+		tooltip.add(new TranslatableText("doom.chaingun.text").formatted(Formatting.ITALIC));
 		super.appendTooltip(stack, world, tooltip, context);
 	}
 
--- src/main/java/mod/azure/doom/item/ammo/ShellAmmo.java
+++ src/main/java/mod/azure/doom/item/ammo/ShellAmmo.java
@@ -11,6 +11,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 
 public class ShellAmmo extends ArrowItem {
@@ -24,7 +25,7 @@ public class ShellAmmo extends ArrowItem {
 
 	@Override
 	public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
-		tooltip.add(new TranslatableText("\u00A7o" + "Loads shotgun with malicious intent."));
+		tooltip.add(new TranslatableText("doom.shell.text").formatted(Formatting.ITALIC));
 		super.appendTooltip(stack, world, tooltip, context);
 	}
 
