52d8fdcb0da9b10feb4e9a83c365671bf6aa7ce1
AzureZhen <rcraft@cyberconnects2.com>
2021-11-15 18:45:12 -0500

    Made Doot Rev rarer

--- src/main/java/mod/azure/doom/entity/tierheavy/Revenant2016Entity.java
+++ src/main/java/mod/azure/doom/entity/tierheavy/Revenant2016Entity.java
@@ -130,7 +130,7 @@ public class Revenant2016Entity extends DemonEntity implements IAnimatable, IAni
 	}
 
 	public int getVariant() {
-		return MathHelper.clamp((Integer) this.dataTracker.get(VARIANT), 1, 2);
+		return MathHelper.clamp((Integer) this.dataTracker.get(VARIANT), 1, 10);
 	}
 
 	public void setVariant(int variant) {
@@ -138,7 +138,7 @@ public class Revenant2016Entity extends DemonEntity implements IAnimatable, IAni
 	}
 
 	public int getVariants() {
-		return 2;
+		return 10;
 	}
 
 	@Override
