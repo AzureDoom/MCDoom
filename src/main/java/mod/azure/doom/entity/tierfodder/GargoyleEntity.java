5922719ea0756d46c39b721633812240139fb61d
AzureZhen <rcraft@cyberconnects2.com>
2021-11-21 13:12:21 -0500

    Revenant 2016 and Gargoyle movements have been redone and are much more aggressive.

--- src/main/java/mod/azure/doom/entity/tierfodder/GargoyleEntity.java
+++ src/main/java/mod/azure/doom/entity/tierfodder/GargoyleEntity.java
@@ -6,8 +6,7 @@ import java.util.Random;
 import mod.azure.doom.entity.DemonEntity;
 import mod.azure.doom.entity.ai.goal.DemonAttackGoal;
 import mod.azure.doom.entity.ai.goal.DemonFlightMoveControl;
-import mod.azure.doom.entity.ai.goal.RangedStrafeAttackGoal;
-import mod.azure.doom.entity.attack.FireballAttack;
+import mod.azure.doom.entity.ai.goal.FlyingRangeAttackGoal;
 import mod.azure.doom.util.registry.ModSoundEvents;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
@@ -108,11 +107,7 @@ public class GargoyleEntity extends DemonEntity implements IAnimatable, IAnimati
 
 	protected void initCustomGoals() {
 		this.goalSelector.add(4,
-				new RangedStrafeAttackGoal(this,
-						new FireballAttack(this, false).setProjectileOriginOffset(0.8, 0.8, 0.8)
-								.setDamage(config.gargoyle_ranged_damage).setSound(SoundEvents.ENTITY_BLAZE_SHOOT, 1.0F,
-										1.4F + this.getRandom().nextFloat() * 0.35F),
-						1.0D, 50, 30, 15, 15F, 1).setMultiShot(3, 3));
+				new FlyingRangeAttackGoal(this, config.gargoyle_ranged_damage, SoundEvents.ENTITY_BLAZE_SHOOT, false));
 		this.goalSelector.add(4, new DemonAttackGoal(this, 1.0D, false, 2));
 		this.targetSelector.add(2, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
 		this.targetSelector.add(2, new ActiveTargetGoal<>(this, MerchantEntity.class, true));
