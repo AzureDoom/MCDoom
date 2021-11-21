b525d4efffc53f0cb6b655ea449cb6f360d66731
AzureZhen <rcraft@cyberconnects2.com>
2021-11-21 13:12:17 -0500

    Revenant 2016 and Gargoyle movements have been redone and are much more aggressive.

--- src/main/java/mod/azure/doom/entity/tierheavy/Revenant2016Entity.java
+++ src/main/java/mod/azure/doom/entity/tierheavy/Revenant2016Entity.java
@@ -3,7 +3,9 @@ package mod.azure.doom.entity.tierheavy;
 import java.util.Random;
 
 import mod.azure.doom.entity.DemonEntity;
-import mod.azure.doom.entity.projectiles.entity.RocketMobEntity;
+import mod.azure.doom.entity.ai.goal.DemonAttackGoal;
+import mod.azure.doom.entity.ai.goal.DemonFlightMoveControl;
+import mod.azure.doom.entity.ai.goal.FlyingRangeAttackGoal;
 import mod.azure.doom.entity.tierfodder.ImpEntity;
 import mod.azure.doom.util.ModSoundEvents;
 import net.minecraft.block.BlockState;
@@ -13,11 +15,12 @@ import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.goal.FollowTargetGoal;
-import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.goal.LookAroundGoal;
 import net.minecraft.entity.ai.goal.LookAtEntityGoal;
 import net.minecraft.entity.ai.goal.RevengeGoal;
 import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
+import net.minecraft.entity.ai.pathing.BirdNavigation;
+import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
@@ -28,12 +31,10 @@ import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
@@ -60,7 +61,7 @@ public class Revenant2016Entity extends DemonEntity implements IAnimatable, IAni
 			event.getController().setAnimation(new AnimationBuilder().addAnimation("walking", true));
 			return PlayState.CONTINUE;
 		}
-		if (this.dataTracker.get(STATE) == 1 || this.hasNoGravity()) {
+		if (!this.isOnGround() && !this.onGround && !(this.dead || this.getHealth() < 0.01 || this.isDead())) {
 			event.getController().setAnimation(new AnimationBuilder().addAnimation("flying", true));
 			return PlayState.CONTINUE;
 		}
@@ -85,6 +86,7 @@ public class Revenant2016Entity extends DemonEntity implements IAnimatable, IAni
 
 	public Revenant2016Entity(EntityType<Revenant2016Entity> entityType, World worldIn) {
 		super(entityType, worldIn);
+		this.moveControl = new DemonFlightMoveControl(this, 90, false);
 	}
 
 	@Override
@@ -94,7 +96,7 @@ public class Revenant2016Entity extends DemonEntity implements IAnimatable, IAni
 
 	public static DefaultAttributeContainer.Builder createMobAttributes() {
 		return LivingEntity.createLivingAttributes().add(EntityAttributes.GENERIC_FOLLOW_RANGE, 25.0D)
-				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25D)
+				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25D).add(EntityAttributes.GENERIC_FLYING_SPEED, 2.25D)
 				.add(EntityAttributes.GENERIC_MAX_HEALTH, config.revenant_health)
 				.add(EntityAttributes.GENERIC_ATTACK_DAMAGE, config.revenant_melee_damage)
 				.add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 1.0D);
@@ -159,100 +161,26 @@ public class Revenant2016Entity extends DemonEntity implements IAnimatable, IAni
 		this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
 		this.goalSelector.add(6, new LookAroundGoal(this));
 		this.targetSelector.add(1, new RevengeGoal(this, new Class[0]).setGroupRevenge());
-		this.goalSelector.add(1, new Revenant2016Entity.FlyingAttackGoal(this));
+		this.goalSelector.add(4, new FlyingRangeAttackGoal(this, config.revenant_ranged_damage,
+				(this.getVariant() == 10 ? ModSoundEvents.REVENANT_DOOT : ModSoundEvents.REVENANT_ATTACK), true));
+		this.goalSelector.add(4, new DemonAttackGoal(this, 1.0D, false, 2));
 		this.targetSelector.add(2, new FollowTargetGoal(this, PlayerEntity.class, true));
 		this.targetSelector.add(3, new FollowTargetGoal<>(this, HostileEntity.class, true));
 	}
 
-	@Override
-	public int getSafeFallDistance() {
-		return 99;
+	protected EntityNavigation createNavigation(World world) {
+		BirdNavigation birdNavigation = new BirdNavigation(this, world);
+		birdNavigation.setCanPathThroughDoors(false);
+		birdNavigation.setCanSwim(true);
+		birdNavigation.setCanEnterOpenDoors(true);
+		return birdNavigation;
 	}
 
-	@Override
-	protected int computeFallDamage(float fallDistance, float damageMultiplier) {
-		return 0;
+	public boolean handleFallDamage(float fallDistance, float damageMultiplier) {
+		return false;
 	}
 
-	@Override
-	protected void updateGoalControls() {
-		boolean flag = this.getTarget() != null && this.canSee(this.getTarget());
-		this.goalSelector.setControlEnabled(Goal.Control.LOOK, flag);
-		super.updateGoalControls();
-	}
-
-	static class FlyingAttackGoal extends Goal {
-		private final Revenant2016Entity parentEntity;
-		protected int attackTimer = 0;
-
-		public FlyingAttackGoal(Revenant2016Entity ghast) {
-			this.parentEntity = ghast;
-		}
-
-		public boolean canStart() {
-			return this.parentEntity.getTarget() != null;
-		}
-
-		public void start() {
-			super.start();
-			this.parentEntity.setAttacking(true);
-			this.attackTimer = 0;
-			this.parentEntity.setAttackingState(0);
-		}
-
-		@Override
-		public void stop() {
-			super.stop();
-			this.parentEntity.setAttacking(false);
-			this.parentEntity.setAttackingState(0);
-			parentEntity.setNoGravity(false);
-			parentEntity.addVelocity(0, 0, 0);
-		}
-
-		public void tick() {
-			LivingEntity livingEntity = this.parentEntity.getTarget();
-			if (this.parentEntity.canSee(livingEntity)) {
-				++this.attackTimer;
-				World world = this.parentEntity.world;
-				Vec3d vec3d = this.parentEntity.getRotationVec(1.0F);
-				double f = livingEntity.getX() - (this.parentEntity.getX() + vec3d.x * 2.0D);
-				double g = livingEntity.getBodyY(0.5D) - (0.5D + this.parentEntity.getBodyY(0.5D));
-				double h = livingEntity.getZ() - (this.parentEntity.getZ() + vec3d.z * 2.0D);
-				RocketMobEntity fireballEntity = new RocketMobEntity(world, this.parentEntity, f, g, h, 5);
-				if (this.attackTimer == 15) {
-					parentEntity.setNoGravity(true);
-					parentEntity.addVelocity(0, (double) 0.2F * 3.0D, 0);
-					this.parentEntity.setAttackingState(1);
-					this.parentEntity.lookAtEntity(livingEntity, 30.0F, 30.0F);
-				}
-				if (this.attackTimer == 15) {
-					fireballEntity.updatePosition(this.parentEntity.getX() + vec3d.x * 2.0D,
-							this.parentEntity.getBodyY(0.5D) + 0.75D, parentEntity.getZ() + vec3d.z * 2.0D);
-					parentEntity.world.playSoundFromEntity(null, parentEntity,
-							(parentEntity.getVariant() == 1 ? ModSoundEvents.REVENANT_ATTACK
-									: ModSoundEvents.REVENANT_DOOT),
-							SoundCategory.HOSTILE, 0.5F, 1.0F);
-					world.spawnEntity(fireballEntity);
-				}
-				if (this.attackTimer == 20) {
-					fireballEntity.updatePosition(this.parentEntity.getX() + vec3d.x * 2.0D,
-							this.parentEntity.getBodyY(0.5D) + 0.75D, parentEntity.getZ() + vec3d.z * 2.0D);
-					parentEntity.world.playSoundFromEntity(null, parentEntity,
-							(parentEntity.getVariant() == 1 ? ModSoundEvents.REVENANT_ATTACK
-									: ModSoundEvents.REVENANT_DOOT),
-							SoundCategory.HOSTILE, 0.5F, 1.0F);
-					world.spawnEntity(fireballEntity);
-				}
-				if (this.attackTimer == 45) {
-					this.parentEntity.setAttackingState(0);
-					parentEntity.setNoGravity(false);
-					parentEntity.addVelocity(0, 0, 0);
-					this.attackTimer = -50;
-				}
-			} else if (this.attackTimer > 0) {
-				--this.attackTimer;
-			}
-		}
+	protected void fall(double heightDifference, boolean onGround, BlockState landedState, BlockPos landedPosition) {
 	}
 
 	protected boolean shouldDrown() {
