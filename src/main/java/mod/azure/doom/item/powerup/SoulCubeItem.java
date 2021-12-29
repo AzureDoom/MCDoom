45847cb58939122645f86adb5d49febe84dcfa83
AzureZhen <rcraft@cyberconnects2.com>
2021-12-29 02:10:57 -0500

    v4.0.37

--- src/main/java/mod/azure/doom/item/powerup/SoulCubeItem.java
+++ src/main/java/mod/azure/doom/item/powerup/SoulCubeItem.java
@@ -1,9 +1,16 @@
 package mod.azure.doom.item.powerup;
 
+import java.util.List;
+
 import dev.emi.trinkets.api.TrinketItem;
 import mod.azure.doom.DoomMod;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
 
 public class SoulCubeItem extends TrinketItem {
 
@@ -14,5 +21,11 @@ public class SoulCubeItem extends TrinketItem {
 	@Override
 	public boolean isEnchantable(ItemStack stack) {
 		return false;
+	}
+
+	@Override
+	public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
+		tooltip.add(new TranslatableText("Uses Remaining: " + stack.getDamage()).formatted(Formatting.ITALIC));
+		super.appendTooltip(stack, world, tooltip, context);
 	}
 }
