cbbfe233597fda0d06c63404cad0525f04f27437
AzureZhen <rcraft@cyberconnects2.com>
2022-01-30 04:35:25 -0500

    Grenade Sound fix.

--- src/main/java/mod/azure/doom/entity/projectiles/GrenadeEntity.java
+++ src/main/java/mod/azure/doom/entity/projectiles/GrenadeEntity.java
@@ -1,6 +1,5 @@
 package mod.azure.doom.entity.projectiles;
 
-import java.util.Iterator;
 import java.util.List;
 
 import mod.azure.doom.DoomMod;
@@ -11,7 +10,6 @@ import mod.azure.doom.util.registry.ModSoundEvents;
 import mod.azure.doom.util.registry.ProjectilesEntityRegister;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.AreaEffectCloudEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -31,13 +29,9 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -166,93 +160,6 @@ public class GrenadeEntity extends PersistentProjectileEntity implements IAnimat
 	@Override
 	public void tick() {
 		super.tick();
-		boolean bl = this.isNoClip();
-		BlockPos blockPos = this.getBlockPos();
-		BlockState blockState = this.world.getBlockState(blockPos);
-		Vec3d vec3d = this.getVelocity();
-		Vec3d vec3d4;
-		if (!blockState.isAir() && !bl) {
-			VoxelShape voxelShape = blockState.getCollisionShape(this.world, blockPos);
-			if (!voxelShape.isEmpty()) {
-				vec3d4 = this.getPos();
-				Iterator<?> var7 = voxelShape.getBoundingBoxes().iterator();
-
-				while (var7.hasNext()) {
-					Box box = (Box) var7.next();
-					if (box.offset(blockPos).contains(vec3d4)) {
-						this.dataTracker.set(SPINNING, false);
-						this.inGround = true;
-						break;
-					}
-				}
-				this.setSpinning(false);
-			}
-		}
-		if (this.prevPitch == 0.0F && this.prevYaw == 0.0F) {
-			double f = vec3d.horizontalLength();
-			this.setYaw((float) (MathHelper.atan2(vec3d.x, vec3d.z) * 57.2957763671875D));
-			this.setPitch((float) (MathHelper.atan2(vec3d.y, f) * 57.2957763671875D));
-			this.prevYaw = this.getYaw();
-			this.prevPitch = this.getPitch();
-		}
-		if (this.age >= 90) {
-			this.remove(Entity.RemovalReason.DISCARDED);
-		}
-		if (this.inAir && !bl) {
-			this.age();
-			++this.timeInAir;
-		} else {
-			this.timeInAir = 0;
-			Vec3d vec3d3 = this.getPos();
-			Vec3d vector3d3 = vec3d3.add(vec3d);
-			HitResult hitResult = this.world.raycast(new RaycastContext(vec3d3, vector3d3,
-					RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, this));
-			if (((HitResult) hitResult).getType() != HitResult.Type.MISS) {
-				vector3d3 = ((HitResult) hitResult).getPos();
-			}
-			while (!this.isRemoved()) {
-				EntityHitResult entityHitResult = this.getEntityCollision(vec3d3, vector3d3);
-				if (entityHitResult != null) {
-					hitResult = entityHitResult;
-				}
-				if (hitResult != null && ((HitResult) hitResult).getType() == HitResult.Type.ENTITY) {
-					Entity entity = ((EntityHitResult) hitResult).getEntity();
-					Entity entity2 = this.getOwner();
-					if (entity instanceof PlayerEntity && entity2 instanceof PlayerEntity
-							&& !((PlayerEntity) entity2).shouldDamagePlayer((PlayerEntity) entity)) {
-						hitResult = null;
-						entityHitResult = null;
-					}
-				}
-				if (hitResult != null && !bl) {
-					this.onCollision((HitResult) hitResult);
-					this.velocityDirty = true;
-				}
-				if (entityHitResult == null || this.getPierceLevel() <= 0) {
-					break;
-				}
-				hitResult = null;
-			}
-			vec3d = this.getVelocity();
-			double e = vec3d.y;
-			double g = vec3d.z;
-			double l = vec3d.horizontalLength();
-			if (bl) {
-				this.setYaw((float) (MathHelper.atan2(-e, -g) * 57.2957763671875D));
-			} else {
-				this.setYaw((float) (MathHelper.atan2(e, g) * 57.2957763671875D));
-			}
-			this.setPitch((float) (MathHelper.atan2(e, l) * 57.2957763671875D));
-			this.setPitch(updateRotation(this.prevPitch, this.getPitch()));
-			this.setYaw(updateRotation(this.prevYaw, this.getYaw()));
-			float m = 0.99F;
-
-			this.setVelocity(vec3d.multiply((double) m));
-			Vec3d vec3d5 = this.getVelocity();
-			this.setVelocity(vec3d5.x, vec3d5.y, vec3d5.z);
-			// this.updatePosition(h, j, k);
-			this.checkBlockCollision();
-		}
 	}
 
 	public void initFromStack(ItemStack stack) {
