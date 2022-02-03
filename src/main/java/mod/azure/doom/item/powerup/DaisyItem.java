42c79def6bf3add5732e8e7a6ec2d1f79b339f76
AzureZhen <rcraft@cyberconnects2.com>
2022-02-03 13:10:51 -0500

    v4.1.4

--- src/main/java/mod/azure/doom/item/powerup/DaisyItem.java
+++ src/main/java/mod/azure/doom/item/powerup/DaisyItem.java
@@ -1,10 +1,19 @@
 package mod.azure.doom.item.powerup;
 
 import java.util.List;
+import java.util.UUID;
 
+import com.google.common.collect.Multimap;
+
+import dev.emi.trinkets.api.SlotAttributes;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketItem;
 import mod.azure.doom.DoomMod;
 import net.minecraft.client.item.TooltipContext;
-//import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
@@ -12,7 +21,7 @@ import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 
-public class DaisyItem extends Item {
+public class DaisyItem extends TrinketItem {
 
 	public DaisyItem() {
 		super(new Item.Settings().group(DoomMod.DoomPowerUPItemGroup).maxCount(1));
@@ -30,13 +39,13 @@ public class DaisyItem extends Item {
 		super.appendTooltip(stack, world, tooltip, context);
 	}
 
-//	public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot,
-//			LivingEntity entity, UUID uuid) {
-//		var modifiers = super.getModifiers(stack, slot, entity, uuid);
-//		modifiers.put(EntityAttributes.GENERIC_MOVEMENT_SPEED, new EntityAttributeModifier(uuid,
-//				DoomMod.MODID + ":movement_speed", 2.0, EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
-//		// If the player has access to ring slots, this will give them an extra one
-//		SlotAttributes.addSlotModifier(modifiers, "legs/belt", uuid, 1, EntityAttributeModifier.Operation.ADDITION);
-//		return modifiers;
-//	}
+	public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot,
+			LivingEntity entity, UUID uuid) {
+		var modifiers = super.getModifiers(stack, slot, entity, uuid);
+		modifiers.put(EntityAttributes.GENERIC_MOVEMENT_SPEED, new EntityAttributeModifier(uuid,
+				DoomMod.MODID + ":movement_speed", 2.0, EntityAttributeModifier.Operation.MULTIPLY_TOTAL));
+		// If the player has access to ring slots, this will give them an extra one
+		SlotAttributes.addSlotModifier(modifiers, "legs/belt", uuid, 1, EntityAttributeModifier.Operation.ADDITION);
+		return modifiers;
+	}
 }
