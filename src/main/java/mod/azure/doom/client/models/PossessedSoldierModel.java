c5d51201bd0f91d1ea3b7ecfe59935ded527ef73
AzureZhen <rcraft@cyberconnects2.com>
2022-02-20 06:44:06 -0500

    v4.1.20

--- src/main/java/mod/azure/doom/client/models/PossessedSoldierModel.java
+++ src/main/java/mod/azure/doom/client/models/PossessedSoldierModel.java
@@ -10,17 +10,27 @@ import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class PossessedSoldierModel extends AnimatedTickingGeoModel<PossessedSoldierEntity> {
 
-	public PossessedSoldierModel() {
-	}
+	private static final ResourceLocation[] TEX = {
+			new ResourceLocation(DoomMod.MODID, "textures/entity/possessedsoldier-eternal_flames_1.png"),
+			new ResourceLocation(DoomMod.MODID, "textures/entity/possessedsoldier-eternal_flames_2.png") };
 
 	@Override
 	public ResourceLocation getModelLocation(PossessedSoldierEntity object) {
-		return new ResourceLocation(DoomMod.MODID, "geo/possessedsoldier.geo.json");
+		return new ResourceLocation(
+				DoomMod.MODID, "geo/"
+						+ (object.getVariant() == 3 ? "possessedsoldier-shield"
+								: object.getVariant() == 2 ? "possessedsoldier-eternal" : "possessedsoldier")
+						+ ".geo.json");
 	}
 
 	@Override
 	public ResourceLocation getTextureLocation(PossessedSoldierEntity object) {
-		return new ResourceLocation(DoomMod.MODID, "textures/entity/possessedsoldier.png");
+		return (object.getVariant() == 2 && object.isAggressive() ? TEX[(object.getFlameTimer())]
+				: object.getVariant() == 2 && !object.isAggressive()
+						? new ResourceLocation(DoomMod.MODID, "textures/entity/possessedsoldier-eternal.png")
+						: object.getVariant() == 3
+								? new ResourceLocation(DoomMod.MODID, "textures/entity/possessedsoldier-shield.png")
+								: new ResourceLocation(DoomMod.MODID, "textures/entity/possessedsoldier.png"));
 	}
 
 	@Override
--- src/main/java/mod/azure/doom/entity/tierfodder/PossessedSoldierEntity.java
+++ src/main/java/mod/azure/doom/entity/tierfodder/PossessedSoldierEntity.java
@@ -1,16 +1,26 @@
 package mod.azure.doom.entity.tierfodder;
 
+import java.util.SplittableRandom;
+
 import mod.azure.doom.entity.DemonEntity;
+import mod.azure.doom.entity.ai.goal.RandomFlyConvergeOnTargetGoal;
 import mod.azure.doom.entity.ai.goal.RangedStrafeAttackGoal;
 import mod.azure.doom.entity.attack.AbstractRangedAttack;
 import mod.azure.doom.entity.attack.AttackSound;
 import mod.azure.doom.entity.projectiles.entity.BarenBlastEntity;
 import mod.azure.doom.util.config.DoomConfig;
 import mod.azure.doom.util.registry.ModSoundEvents;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Mth;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityDimensions;
@@ -18,21 +28,29 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.MobType;
+import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.control.MoveControl;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.animal.IronGolem;
 import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.network.NetworkHooks;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.IAnimationTickable;
@@ -46,11 +64,15 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 public class PossessedSoldierEntity extends DemonEntity implements IAnimatable, IAnimationTickable {
 
+	private AnimationFactory factory = new AnimationFactory(this);
+	public static final EntityDataAccessor<Integer> VARIANT = SynchedEntityData.defineId(PossessedSoldierEntity.class,
+			EntityDataSerializers.INT);
+	public int flameTimer;
+
 	public PossessedSoldierEntity(EntityType<PossessedSoldierEntity> entityType, Level worldIn) {
 		super(entityType, worldIn);
+		this.moveControl = new SoldierMoveControl(this);
 	}
-
-	private AnimationFactory factory = new AnimationFactory(this);
 
 	private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
 		if (event.isMoving()) {
@@ -63,6 +85,11 @@ public class PossessedSoldierEntity extends DemonEntity implements IAnimatable, 
 		}
 		if (!event.isMoving() && this.hurtMarked) {
 			event.getController().setAnimation(new AnimationBuilder().addAnimation("idle", true));
+			return PlayState.CONTINUE;
+		}
+		if (!this.isOnGround() && !this.onGround && this.getVariant() == 2
+				&& !(this.dead || this.getHealth() < 0.01 || this.isDeadOrDying())) {
+			event.getController().setAnimation(new AnimationBuilder().addAnimation("flying", true));
 			return PlayState.CONTINUE;
 		}
 		event.getController().setAnimation(new AnimationBuilder().addAnimation("idle", true));
@@ -115,6 +142,46 @@ public class PossessedSoldierEntity extends DemonEntity implements IAnimatable, 
 	}
 
 	@Override
+	protected void defineSynchedData() {
+		super.defineSynchedData();
+		this.entityData.define(VARIANT, 0);
+	}
+
+	@Override
+	public void readAdditionalSaveData(CompoundTag tag) {
+		super.readAdditionalSaveData(tag);
+		this.setVariant(tag.getInt("Variant"));
+	}
+
+	@Override
+	public void addAdditionalSaveData(CompoundTag tag) {
+		super.addAdditionalSaveData(tag);
+		tag.putInt("Variant", this.getVariant());
+	}
+
+	public int getVariant() {
+		return Mth.clamp((Integer) this.entityData.get(VARIANT), 1, 3);
+	}
+
+	public void setVariant(int variant) {
+		this.entityData.set(VARIANT, variant);
+	}
+
+	public int getVariants() {
+		return 3;
+	}
+
+	@Override
+	public SpawnGroupData finalizeSpawn(ServerLevelAccessor worldIn, DifficultyInstance difficultyIn,
+			MobSpawnType reason, SpawnGroupData spawnDataIn, CompoundTag dataTag) {
+		spawnDataIn = super.finalizeSpawn(worldIn, difficultyIn, reason, spawnDataIn, dataTag);
+		SplittableRandom random = new SplittableRandom();
+		int var = random.nextInt(0, 4);
+		this.setVariant(var);
+		return spawnDataIn;
+	}
+
+	@Override
 	protected void registerGoals() {
 		this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8.0F));
 		this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
@@ -124,11 +191,129 @@ public class PossessedSoldierEntity extends DemonEntity implements IAnimatable, 
 						new PossessedSoldierEntity.FireballAttack(this).setProjectileOriginOffset(0.8, 0.8, 0.8)
 								.setDamage(DoomConfig.SERVER.possessed_soldier_ranged_damage.get().floatValue()),
 						1.0D, 5, 30, 15, 15F, 1));
+		if (this.getVariant() == 2) {
+			this.goalSelector.addGoal(5, new RandomFlyConvergeOnTargetGoal(this, 2, 15, 0.5));
+		}
 		this.targetSelector.addGoal(1, new HurtByTargetGoal(this).setAlertOthers());
 		this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
 		this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, false));
 		this.targetSelector.addGoal(1, (new HurtByTargetGoal(this).setAlertOthers()));
 		this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolem.class, true));
+	}
+
+	public void travel(Vec3 movementInput) {
+		if (this.isAggressive() && this.getVariant() == 2) {
+			if (this.isInWater()) {
+				this.moveRelative(0.02F, movementInput);
+				this.move(MoverType.SELF, this.getDeltaMovement());
+				this.setDeltaMovement(this.getDeltaMovement().scale((double) 0.8F));
+			} else if (this.isInLava()) {
+				this.moveRelative(0.02F, movementInput);
+				this.move(MoverType.SELF, this.getDeltaMovement());
+				this.setDeltaMovement(this.getDeltaMovement().scale(0.5D));
+			} else {
+				BlockPos ground = new BlockPos(this.getX(), this.getY() - 1.0D, this.getZ());
+				float f = 0.91F;
+				if (this.onGround) {
+					f = this.level.getBlockState(ground).getFriction(this.level, ground, this) * 0.91F;
+				}
+				float f1 = 0.16277137F / (f * f * f);
+				f = 0.91F;
+				if (this.onGround) {
+					f = this.level.getBlockState(ground).getFriction(this.level, ground, this) * 0.91F;
+				}
+				this.moveRelative(this.onGround ? 0.1F * f1 : 0.02F, movementInput);
+				this.move(MoverType.SELF, this.getDeltaMovement());
+				this.setDeltaMovement(this.getDeltaMovement().scale((double) f));
+			}
+			this.calculateEntityAnimation(this, false);
+		} else {
+			super.travel(movementInput);
+		}
+	}
+
+	static class SoldierMoveControl extends MoveControl {
+		protected final PossessedSoldierEntity entity;
+		private int courseChangeCooldown;
+
+		public SoldierMoveControl(PossessedSoldierEntity entity) {
+			super(entity);
+			this.entity = entity;
+		}
+
+		public void tick() {
+			if (entity.isAggressive() && this.entity.getVariant() == 2) {
+				if (this.operation == MoveControl.Operation.MOVE_TO) {
+					if (this.courseChangeCooldown-- <= 0) {
+						this.courseChangeCooldown += this.entity.getRandom().nextInt(5) + 2;
+						Vec3 vector3d = new Vec3(this.wantedX - this.entity.getX(), this.wantedY - this.entity.getY(),
+								this.wantedZ - this.entity.getZ());
+						double d0 = vector3d.length();
+						vector3d = vector3d.normalize();
+						if (this.canReach(vector3d, Mth.ceil(d0))) {
+							this.entity.setDeltaMovement(this.entity.getDeltaMovement().add(vector3d.scale(0.1D)));
+						} else {
+							this.operation = MoveControl.Operation.WAIT;
+						}
+					}
+				} else {
+					this.operation = MoveControl.Operation.WAIT;
+					this.entity.setZza(0.0F);
+				}
+			} else {
+				if (this.operation == MoveControl.Operation.MOVE_TO) {
+					this.operation = MoveControl.Operation.WAIT;
+					double d0 = this.wantedX - this.entity.getX();
+					double d1 = this.wantedZ - this.entity.getZ();
+					double d2 = this.wantedY - this.entity.getY();
+					double d3 = d0 * d0 + d2 * d2 + d1 * d1;
+					if (d3 < (double) 2.5000003E-7F) {
+						this.entity.setZza(0.0F);
+						return;
+					}
+					float f9 = (float) (Mth.atan2(d1, d0) * (double) (180F / (float) Math.PI)) - 90.0F;
+					this.entity.setYRot(this.rotlerp(this.mob.getYRot(), f9, 90.0F));
+					this.entity.setSpeed((float) (0.25D));
+					BlockPos blockpos = this.mob.blockPosition();
+					BlockState blockstate = this.mob.level.getBlockState(blockpos);
+					VoxelShape voxelshape = blockstate.getCollisionShape(this.mob.level, blockpos);
+					if (d2 > (double) this.mob.maxUpStep
+							&& d0 * d0 + d1 * d1 < (double) Math.max(1.0F, this.mob.getBbWidth())
+							|| !voxelshape.isEmpty()
+									&& this.mob.getY() < voxelshape.max(Direction.Axis.Y) + (double) blockpos.getY()
+									&& !blockstate.is(BlockTags.DOORS) && !blockstate.is(BlockTags.FENCES)) {
+						this.operation = MoveControl.Operation.JUMPING;
+					}
+				} else if (this.operation == MoveControl.Operation.JUMPING) {
+					this.mob.setSpeed((float) (0.25D));
+					if (this.mob.isOnGround()) {
+						this.operation = MoveControl.Operation.WAIT;
+					}
+				} else {
+					this.operation = MoveControl.Operation.WAIT;
+					this.entity.setZza(0.0F);
+				}
+			}
+		}
+
+		private boolean canReach(Vec3 direction, int steps) {
+			AABB axisalignedbb = this.mob.getBoundingBox();
+			for (int i = 1; i < steps; ++i) {
+				axisalignedbb = axisalignedbb.move(direction);
+				if (!this.mob.level.noCollision(this.entity, axisalignedbb)) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+	protected PathNavigation createNavigation(Level worldIn) {
+		FlyingPathNavigation flyingpathnavigator = new FlyingPathNavigation(this, worldIn);
+		flyingpathnavigator.setCanOpenDoors(false);
+		flyingpathnavigator.setCanFloat(true);
+		flyingpathnavigator.setCanPassDoors(true);
+		return flyingpathnavigator;
 	}
 
 	public class FireballAttack extends AbstractRangedAttack {
@@ -153,11 +338,6 @@ public class PossessedSoldierEntity extends DemonEntity implements IAnimatable, 
 		}
 	}
 
-	@Override
-	public void readAdditionalSaveData(CompoundTag compound) {
-		super.readAdditionalSaveData(compound);
-	}
-
 	public static AttributeSupplier.Builder createAttributes() {
 		return LivingEntity.createLivingAttributes().add(Attributes.FOLLOW_RANGE, 25.0D)
 				.add(Attributes.MAX_HEALTH, DoomConfig.SERVER.possessed_soldier_health.get())
@@ -168,14 +348,6 @@ public class PossessedSoldierEntity extends DemonEntity implements IAnimatable, 
 	@Override
 	protected float getStandingEyeHeight(Pose poseIn, EntityDimensions sizeIn) {
 		return 1.74F;
-	}
-
-	@Override
-	public SpawnGroupData finalizeSpawn(ServerLevelAccessor worldIn, DifficultyInstance difficultyIn,
-			MobSpawnType reason, SpawnGroupData spawnDataIn, CompoundTag dataTag) {
-		spawnDataIn = super.finalizeSpawn(worldIn, difficultyIn, reason, spawnDataIn, dataTag);
-		this.populateDefaultEquipmentSlots(difficultyIn);
-		return spawnDataIn;
 	}
 
 	@Override
@@ -201,6 +373,16 @@ public class PossessedSoldierEntity extends DemonEntity implements IAnimatable, 
 	@Override
 	public int tickTimer() {
 		return tickCount;
+	}
+
+	@Override
+	public void aiStep() {
+		super.aiStep();
+		flameTimer = (flameTimer + 1) % 2;
+	}
+
+	public int getFlameTimer() {
+		return flameTimer;
 	}
 
 }
