2122eed9f4427e9f9a1289c9537fba75a0aa96d9
AzureZhen <rcraft@cyberconnects2.com>
2022-05-21 08:59:34 -0400

    REI support added

---
+++ src/main/java/mod/azure/doom/rei/DoomDisplay.java
@@ -0,0 +1,42 @@
+package mod.azure.doom.rei;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.Display;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import mod.azure.doom.recipes.GunTableRecipe;
+
+public class DoomDisplay implements Display {
+	public final List<EntryIngredient> input;
+	public final List<Integer> count;
+	public final EntryIngredient output;
+	public final GunTableRecipe recipe2;
+
+	public DoomDisplay(GunTableRecipe recipe) {
+		input = Arrays.stream(recipe.ingredients).map(Pair::getLeft).map(EntryIngredients::ofIngredient).toList();
+		count = Arrays.stream(recipe.ingredients).map(Pair::getRight).toList();
+		this.output = EntryIngredients.of(recipe.getResultItem());
+		this.recipe2 = recipe;
+	}
+
+	@Override
+	public List<EntryIngredient> getInputEntries() {
+		return Arrays.stream(recipe2.ingredients).map(Pair::getLeft).map(EntryIngredients::ofIngredient).toList();
+	}
+
+	@Override
+	public List<EntryIngredient> getOutputEntries() {
+		return Collections.singletonList(output);
+	}
+
+	@Override
+	public CategoryIdentifier<?> getCategoryIdentifier() {
+		return ReiPlugin.CRAFTING;
+	}
+}
---
+++ src/main/java/mod/azure/doom/rei/ReiPlugin.java
@@ -0,0 +1,30 @@
+package mod.azure.doom.rei;
+
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.forge.REIPlugin;
+import mod.azure.doom.DoomMod;
+import mod.azure.doom.recipes.GunTableRecipe;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+
+@REIPlugin(value = Dist.CLIENT)
+public class ReiPlugin implements REIClientPlugin {
+
+	public static final CategoryIdentifier<DoomDisplay> CRAFTING = CategoryIdentifier
+			.of(new ResourceLocation(DoomMod.MODID, "crafting"));
+
+	@Override
+	public void registerCategories(CategoryRegistry registry) {
+		registry.add(new DoomCategory());
+		registry.addWorkstations(CRAFTING, DoomCategory.ICON);
+		registry.removePlusButton(CRAFTING);
+	}
+
+	@Override
+	public void registerDisplays(DisplayRegistry registry) {
+		registry.registerFiller(GunTableRecipe.class, DoomDisplay::new);
+	}
+}
