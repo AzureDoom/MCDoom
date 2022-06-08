54ab51eb982667338fc429afecb17638de5c6fdf
AzureZhen <rcraft@cyberconnects2.com>
2022-06-08 17:52:29 -0400

    v.4.2.0b

--- src/main/java/mod/azure/doom/rei/ReiPlugin.java
+++ src/main/java/mod/azure/doom/rei/ReiPlugin.java
@@ -1,19 +1,29 @@
 package mod.azure.doom.rei;
 
-//@REIPlugin(value = Dist.CLIENT)
-public class ReiPlugin {// implements REIClientPlugin {
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.forge.REIPlugin;
+import mod.azure.doom.DoomMod;
+import mod.azure.doom.recipes.GunTableRecipe;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
 
-//	public static final CategoryIdentifier<DoomDisplay> CRAFTING = CategoryIdentifier
-//			.of(new ResourceLocation(DoomMod.MODID, "crafting"));
-//
-//	@Override
-//	public void registerCategories(CategoryRegistry registry) {
-//		registry.add(new DoomCategory());
-//		registry.addWorkstations(CRAFTING, DoomCategory.ICON);
-//	}
-//
-//	@Override
-//	public void registerDisplays(DisplayRegistry registry) {
-//		registry.registerFiller(GunTableRecipe.class, DoomDisplay::new);
-//	}
+@REIPlugin(value = Dist.CLIENT)
+public class ReiPlugin implements REIClientPlugin {
+
+	public static final CategoryIdentifier<DoomDisplay> CRAFTING = CategoryIdentifier
+			.of(new ResourceLocation(DoomMod.MODID, "crafting"));
+
+	@Override
+	public void registerCategories(CategoryRegistry registry) {
+		registry.add(new DoomCategory());
+		registry.addWorkstations(CRAFTING, DoomCategory.ICON);
+	}
+
+	@Override
+	public void registerDisplays(DisplayRegistry registry) {
+		registry.registerFiller(GunTableRecipe.class, DoomDisplay::new);
+	}
 }
