c4bef2f500a5bcc5348c8c48b27e1a558e050e34
AzureZhen <rcraft@cyberconnects2.com>
2022-06-26 07:04:54 -0400

    Cyber Mancubus Variant added to Mancubus. - Model tweak/Texture by MangoDragon21

--- src/main/java/mod/azure/doom/client/models/MancubusModel.java
+++ src/main/java/mod/azure/doom/client/models/MancubusModel.java
@@ -2,8 +2,8 @@ package mod.azure.doom.client.models;
 
 import mod.azure.doom.DoomMod;
 import mod.azure.doom.entity.tierheavy.MancubusEntity;
-import net.minecraft.util.math.Vec3f;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3f;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedTickingGeoModel;
@@ -21,7 +21,8 @@ public class MancubusModel extends AnimatedTickingGeoModel<MancubusEntity> {
 
 	@Override
 	public Identifier getTextureResource(MancubusEntity object) {
-		return new Identifier(DoomMod.MODID, "textures/entity/mancubus.png");
+		return new Identifier(DoomMod.MODID,
+				"textures/entity/" + (object.getVariant() == 2 ? "cyber_mancubus" : "mancubus") + ".png");
 	}
 
 	@Override
