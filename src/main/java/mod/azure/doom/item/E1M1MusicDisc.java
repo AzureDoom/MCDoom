729d499fd2ec6be70ad51a9e113fe01aa621467c
AzureZhen <rcraft@cyberconnects2.com>
2022-07-28 10:36:38 -0400

    1.19.1

--- src/main/java/mod/azure/doom/item/E1M1MusicDisc.java
+++ src/main/java/mod/azure/doom/item/E1M1MusicDisc.java
@@ -3,16 +3,16 @@ package mod.azure.doom.item;
 import java.util.function.Supplier;
 
 import mod.azure.doom.DoomMod;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.RecordItem;
-import net.minecraft.world.item.Rarity;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.RecordItem;
 
 public class E1M1MusicDisc extends RecordItem {
 
 	public E1M1MusicDisc(Supplier<SoundEvent> soundSupplier) {
 		super(1, soundSupplier,
-				(new Item.Properties()).stacksTo(1).tab(DoomMod.DoomPowerUPItemGroup).rarity(Rarity.RARE));
+				(new Item.Properties()).stacksTo(1).tab(DoomMod.DoomPowerUPItemGroup).rarity(Rarity.RARE), 60);
 	}
 
 }
