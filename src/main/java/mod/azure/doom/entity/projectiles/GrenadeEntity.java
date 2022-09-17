38c3aa541ead91039d36a174507e7088ebc8df72
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-09-17 12:32:41 -0400

    v4.2.16

--- src/main/java/mod/azure/doom/entity/projectiles/GrenadeEntity.java
+++ src/main/java/mod/azure/doom/entity/projectiles/GrenadeEntity.java
@@ -19,6 +19,7 @@ import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.Packet;
 import net.minecraft.particle.ParticleTypes;
@@ -66,7 +67,7 @@ public class GrenadeEntity extends PersistentProjectileEntity implements IAnimat
 		this.setOwner(owner);
 		this.shooter = owner;
 		if (owner instanceof PlayerEntity) {
-			this.pickupType = PersistentProjectileEntity.PickupPermission.ALLOWED;
+			this.pickupType = PersistentProjectileEntity.PickupPermission.DISALLOWED;
 		}
 
 	}
@@ -215,7 +216,7 @@ public class GrenadeEntity extends PersistentProjectileEntity implements IAnimat
 
 	@Override
 	public ItemStack asItemStack() {
-		return new ItemStack(DoomItems.GRENADE);
+		return new ItemStack(Items.AIR);
 	}
 
 	@Override
