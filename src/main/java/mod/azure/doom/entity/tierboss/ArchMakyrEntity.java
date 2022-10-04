183587fa19f00c5b35d4c45e998d5ee561195f5b
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-10-04 10:44:13 -0400

    v4.2.17

--- src/main/java/mod/azure/doom/entity/tierboss/ArchMakyrEntity.java
+++ src/main/java/mod/azure/doom/entity/tierboss/ArchMakyrEntity.java
@@ -1,6 +1,5 @@
 package mod.azure.doom.entity.tierboss;
 
-import java.util.List;
 import java.util.Random;
 
 import org.jetbrains.annotations.Nullable;
@@ -418,21 +417,12 @@ public class ArchMakyrEntity extends DemonEntity implements IAnimatable, IAnimat
 	@Override
 	public void baseTick() {
 		super.baseTick();
-		float q = 300.0F;
-		int k = MathHelper.floor(this.getX() - (double) q - 1.0D);
-		int l = MathHelper.floor(this.getX() + (double) q + 1.0D);
-		int t = MathHelper.floor(this.getY() - (double) q - 1.0D);
-		int u = MathHelper.floor(this.getY() + (double) q + 1.0D);
-		int v = MathHelper.floor(this.getZ() - (double) q - 1.0D);
-		int w = MathHelper.floor(this.getZ() + (double) q + 1.0D);
-		List<Entity> list = this.world.getOtherEntities(this,
-				new Box((double) k, (double) t, (double) v, (double) l, (double) u, (double) w));
-		for (int x = 0; x < list.size(); ++x) {
-			Entity entity = (Entity) list.get(x);
-			if (entity instanceof ArchMakyrEntity && entity.age < 1) {
-				entity.remove(Entity.RemovalReason.DISCARDED);
+		final Box aabb = new Box(this.getBlockPos().up()).expand(64D, 64D, 64D);
+		this.getCommandSenderWorld().getOtherEntities(this, aabb).forEach(e -> {
+			if (e instanceof ArchMakyrEntity && e.age < 1) {
+				e.remove(RemovalReason.KILLED);
 			}
-		}
+		});
 	}
 
 	public void onStartedTrackingBy(ServerPlayerEntity player) {
