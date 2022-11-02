7f009daa44cb4a392d93bd0a3899ec86427759df
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-02 02:13:04 -0400

    Pinky/Spectre Speed Buff

--- src/main/java/mod/azure/doom/entity/tierheavy/SpectreEntity.java
+++ src/main/java/mod/azure/doom/entity/tierheavy/SpectreEntity.java
@@ -134,7 +134,7 @@ public class SpectreEntity extends DemonEntity implements IAnimatable, IAnimatio
 		this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
 		this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
 		this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-		this.goalSelector.addGoal(4, new DemonAttackGoal(this, 1.25D, 1));
+		this.goalSelector.addGoal(4, new DemonAttackGoal(this, 1.75D, 1));
 		this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
 		this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, false));
 		this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolem.class, true));
