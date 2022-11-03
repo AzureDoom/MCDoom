3396996548aa9a7c05d9c909e520638af28a92cf
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-03 13:11:50 -0400

    Pinky and Spectre Knockback buff

--- src/main/java/mod/azure/doom/entity/tierheavy/SpectreEntity.java
+++ src/main/java/mod/azure/doom/entity/tierheavy/SpectreEntity.java
@@ -134,7 +134,7 @@ public class SpectreEntity extends DemonEntity implements IAnimatable, IAnimatio
 				.add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 0.6f)
 				.add(EntityAttributes.GENERIC_ATTACK_DAMAGE, DoomConfig.spectre_melee_damage)
 				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25D)
-				.add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 1.0D);
+				.add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 10.0D);
 	}
 
 	@Override
