fc9da3e41c22971e419a3c5127bf951b04106c57
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-03 14:14:50 -0400

    Pain Elementals now spawn up to 16 Lost Souls in an area. If 16 or more is detected in an area, it will stop spawning.

--- src/main/java/mod/azure/doom/entity/ai/goal/PainAttackGoal.java
+++ src/main/java/mod/azure/doom/entity/ai/goal/PainAttackGoal.java
@@ -6,8 +6,10 @@ import mod.azure.doom.entity.tierfodder.LostSoulEntity;
 import mod.azure.doom.entity.tierheavy.PainEntity;
 import mod.azure.doom.util.registry.DoomEntities;
 import mod.azure.doom.util.registry.DoomSounds;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.phys.AABB;
 
 public class PainAttackGoal extends Goal {
 	private final PainEntity entity;
@@ -21,6 +23,11 @@ public class PainAttackGoal extends Goal {
 	@Override
 	public boolean canUse() {
 		return this.entity.getTarget() != null;
+	}
+
+	@Override
+	public boolean canContinueToUse() {
+		return this.canUse();
 	}
 
 	@Override
@@ -45,51 +52,53 @@ public class PainAttackGoal extends Goal {
 			boolean inLineOfSight = this.entity.getSensing().hasLineOfSight(livingentity);
 			this.attackTime++;
 			this.entity.lookAt(livingentity, 30.0F, 30.0F);
-			double d0 = this.entity.distanceToSqr(livingentity.getX(), livingentity.getY(), livingentity.getZ());
-			double d1 = (double) (this.entity.getBbWidth() * 2.0F * this.entity.getBbWidth() * 2.0F
-					+ entity.getBbWidth());
+			final AABB aabb = new AABB(this.entity.blockPosition()).inflate(64D);
+			final AABB aabb2 = new AABB(this.entity.blockPosition()).inflate(3D);
 			if (inLineOfSight) {
-				if (this.entity.distanceTo(livingentity) >= 12.0D) {
+				if (this.entity.distanceTo(livingentity) >= 3.0D) {
 					this.entity.getNavigation().moveTo(livingentity, 1);
-					this.entity.getLookControl().setLookAt(livingentity.getX(), livingentity.getEyeY(),
-							livingentity.getZ());
-					if (this.attackTime == 1) {
-						this.entity.setAttackingState(0);
-					}
-					if (this.attackTime == 12) {
-						this.entity.setAttackingState(1);
-						entity.playSound(DoomSounds.PAIN_HURT.get(), 1.0F, 1.0F);
-						if (this.entity.getVariant() == 1 || this.entity.getVariant() == 3) {
-							LostSoulEntity lost_soul = DoomEntities.LOST_SOUL.get().create(entity.level);
-							lost_soul.moveTo(this.entity.getX(), this.entity.getY(), this.entity.getZ(), 0, 0);
-							entity.level.addFreshEntity(lost_soul);
-						} else {
-							LostSoulEntity lost_soul = DoomEntities.LOST_SOUL.get().create(entity.level);
-							lost_soul.moveTo(this.entity.getX(), this.entity.getY(), this.entity.getZ(), 0, 0);
-							entity.level.addFreshEntity(lost_soul);
-
-							LostSoulEntity lost_soul1 = DoomEntities.LOST_SOUL.get().create(entity.level);
-							lost_soul1.moveTo(this.entity.getX(), this.entity.getY(), this.entity.getZ(), 0, 0);
-							entity.level.addFreshEntity(lost_soul1);
+					int i = this.entity.level.getEntities(DoomEntities.LOST_SOUL.get(), aabb, Entity::isAlive).size();
+					if (i <= 15) {
+						if (this.attackTime == 1) {
+							this.entity.setAttackingState(0);
 						}
+						if (this.attackTime == 12) {
+							this.entity.setAttackingState(1);
+							entity.playSound(DoomSounds.PAIN_HURT.get(), 1.0F, 1.0F);
+							if (this.entity.getVariant() == 1 || this.entity.getVariant() == 3) {
+								LostSoulEntity lost_soul = DoomEntities.LOST_SOUL.get().create(entity.level);
+								lost_soul.moveTo(this.entity.getX(), this.entity.getY(), this.entity.getZ(), 0, 0);
+								entity.level.addFreshEntity(lost_soul);
+							} else {
+								LostSoulEntity lost_soul = DoomEntities.LOST_SOUL.get().create(entity.level);
+								lost_soul.moveTo(this.entity.getX(), this.entity.getY(), this.entity.getZ(), 0, 0);
+								entity.level.addFreshEntity(lost_soul);
 
-						boolean isInsideWaterBlock = entity.level.isWaterAt(entity.blockPosition());
-						entity.spawnLightSource(this.entity, isInsideWaterBlock);
-					}
-					if (this.attackTime == 20) {
-						this.attackTime = 0;
+								LostSoulEntity lost_soul1 = DoomEntities.LOST_SOUL.get().create(entity.level);
+								lost_soul1.moveTo(this.entity.getX(), this.entity.getY(), this.entity.getZ(), 0, 0);
+								entity.level.addFreshEntity(lost_soul1);
+							}
+
+							boolean isInsideWaterBlock = entity.level.isWaterAt(entity.blockPosition());
+							entity.spawnLightSource(this.entity, isInsideWaterBlock);
+						}
+						if (this.attackTime >= 20) {
+							this.attackTime = 0;
+							this.entity.setAttackingState(0);
+						}
+					} else {
+						--this.attackTime;
 						this.entity.setAttackingState(0);
 					}
 				} else {
-					this.entity.getLookControl().setLookAt(livingentity.getX(), livingentity.getEyeY(),
-							livingentity.getZ());
 					if (this.attackTime == 3) {
-						if (d0 <= d1) {
-							this.entity.setAttackingState(1);
-							this.entity.doHurtTarget(livingentity);
-						}
+						this.entity.getCommandSenderWorld().getEntities(this.entity, aabb2).forEach(e -> {
+							if ((e instanceof LivingEntity)) {
+								this.entity.doHurtTarget(livingentity);
+							}
+						});
 					}
-					if (this.attackTime == 20) {
+					if (this.attackTime >= 10) {
 						this.attackTime = 0;
 						this.entity.setAttackingState(0);
 					}
