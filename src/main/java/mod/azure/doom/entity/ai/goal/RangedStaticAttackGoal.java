a95f77750de7909c5abe1446844545eea707aaea
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-03 13:06:59 -0400

    clean up ranged static attacking/looking

--- src/main/java/mod/azure/doom/entity/ai/goal/RangedStaticAttackGoal.java
+++ src/main/java/mod/azure/doom/entity/ai/goal/RangedStaticAttackGoal.java
@@ -10,19 +10,12 @@ public class RangedStaticAttackGoal extends Goal {
 	private final DemonEntity parentEntity;
 	public int attackTimer;
 	private AbstractRangedAttack attack;
-	private int attackCooldown;
-	private int visibleTicksDelay = 20;
-	private float maxAttackDistance = 20;
 	private int seeTime = -1;
 	private int statecheck;
 
-	public RangedStaticAttackGoal(DemonEntity mob, AbstractRangedAttack attack, int attackCooldownIn,
-			int visibleTicksDelay, float maxAttackDistanceIn, int state) {
+	public RangedStaticAttackGoal(DemonEntity mob, AbstractRangedAttack attack, int state) {
 		this.parentEntity = mob;
-		this.attackCooldown = attackCooldownIn;
-		this.maxAttackDistance = maxAttackDistanceIn * maxAttackDistanceIn;
 		this.attack = attack;
-		this.visibleTicksDelay = visibleTicksDelay;
 		this.statecheck = state;
 	}
 
@@ -48,6 +41,7 @@ public class RangedStaticAttackGoal extends Goal {
 	public void tick() {
 		if (this.parentEntity.getTarget() != null) {
 			LivingEntity livingentity = this.parentEntity.getTarget();
+			this.parentEntity.lookAtEntity(livingentity, 30.0F, 30.0F);
 			boolean inLineOfSight = this.parentEntity.getVisibilityCache().canSee(livingentity);
 			if (inLineOfSight != this.seeTime > 0) {
 				this.seeTime = 0;
@@ -57,24 +51,22 @@ public class RangedStaticAttackGoal extends Goal {
 			} else {
 				--this.seeTime;
 			}
-			if (livingentity.squaredDistanceTo(this.parentEntity) < this.maxAttackDistance
-					&& this.seeTime >= this.visibleTicksDelay) {
-				this.parentEntity.getLookControl().lookAt(livingentity, 90.0F, 30.0F);
-				++this.attackTimer;
-
-				if (this.attackTimer == this.attackCooldown) {
-					this.parentEntity.setAttackingState(statecheck);
-					attack.shoot();
+			this.parentEntity.getNavigation().startMovingTo(livingentity, 0.95F);
+			this.parentEntity.getMoveControl().strafeTo(0.5F, -0.5F);
+			this.attackTimer++;
+			if (this.attackTimer == 1) {
+				this.parentEntity.setAttackingState(statecheck);
+			}
+			if (this.attackTimer == 4) {
+					this.attack.shoot();
 
 					boolean isInsideWaterBlock = parentEntity.world.isWater(parentEntity.getBlockPos());
 					parentEntity.spawnLightSource(this.parentEntity, isInsideWaterBlock);
-					this.attackTimer = 0;
-				}
-			} else if (this.attackTimer > 0) {
-				--this.attackTimer;
-				this.parentEntity.setAttackingState(0);
 			}
-			this.parentEntity.getNavigation().startMovingTo(livingentity, 1.4);
+			if (this.attackTimer >= 8) {
+				this.parentEntity.setAttackingState(0);
+				this.attackTimer = -5;
+			}
 		}
 	}
 
