b302c5f38986f329e221fcdd3f78dc6a101a0227
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-06 18:17:11 -0500

    Doom 64 Unmaker added as new item

---
+++ src/main/java/mod/azure/doom/client/models/weapons/UnmakerModel.java
@@ -0,0 +1,23 @@
+package mod.azure.doom.client.models.weapons;
+
+import mod.azure.doom.DoomMod;
+import mod.azure.doom.item.weapons.Unmaykr;
+import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+public class UnmakerModel extends AnimatedGeoModel<Unmaykr> {
+	@Override
+	public Identifier getModelResource(Unmaykr object) {
+		return new Identifier(DoomMod.MODID, "geo/unmaykr.geo.json");
+	}
+
+	@Override
+	public Identifier getTextureResource(Unmaykr object) {
+		return new Identifier(DoomMod.MODID, "textures/items/unmaker.png");
+	}
+
+	@Override
+	public Identifier getAnimationResource(Unmaykr animatable) {
+		return new Identifier(DoomMod.MODID, "animations/unmaykr.animation.json");
+	}
+}
