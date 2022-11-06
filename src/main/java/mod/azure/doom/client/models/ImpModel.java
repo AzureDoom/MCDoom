3fcb3da0afe61c5ff105db0d29d3c8efdfd539a8
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-06 17:10:47 -0500

    Doom 64 Imp Variant added/Nightmare Imp model tweaked

--- src/main/java/mod/azure/doom/client/models/ImpModel.java
+++ src/main/java/mod/azure/doom/client/models/ImpModel.java
@@ -1,9 +1,10 @@
 package mod.azure.doom.client.models;
 
+import com.mojang.math.Vector3f;
+
 import mod.azure.doom.DoomMod;
 import mod.azure.doom.entity.tierfodder.ImpEntity;
 import net.minecraft.resources.ResourceLocation;
-import com.mojang.math.Vector3f;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedTickingGeoModel;
@@ -14,26 +15,32 @@ public class ImpModel extends AnimatedTickingGeoModel<ImpEntity> {
 	public ResourceLocation classic_model = new ResourceLocation(DoomMod.MODID, "geo/imp.geo.json");
 	public ResourceLocation nightmareimp_model = new ResourceLocation(DoomMod.MODID, "geo/nightmareimp.geo.json");
 	public ResourceLocation imp2016_model = new ResourceLocation(DoomMod.MODID, "geo/imp2016.geo.json");
+	
 	public ResourceLocation classic_texture = new ResourceLocation(DoomMod.MODID, "textures/entity/imp-texturemap.png");
+	public ResourceLocation d64_texture = new ResourceLocation(DoomMod.MODID, "textures/entity/imp-64.png");
 	public ResourceLocation nightmareimp_texture = new ResourceLocation(DoomMod.MODID, "textures/entity/nightmareimp-texture.png");
 	public ResourceLocation imp2016_texture = new ResourceLocation(DoomMod.MODID, "textures/entity/imp2016.png");
+	
 	public ResourceLocation imp2016_animation = new ResourceLocation(DoomMod.MODID, "animations/imp2016.animation.json");
 	public ResourceLocation imp_animation = new ResourceLocation(DoomMod.MODID, "animations/imp_animation.json");
 
 	@Override
 	public ResourceLocation getModelResource(ImpEntity object) {
-		return object.getVariant() == 2 ? nightmareimp_model : object.getVariant() == 3 ? imp2016_model : classic_model;
+		return object.getVariant() == 2 ? nightmareimp_model
+				: object.getVariant() == 3 ? nightmareimp_model
+						: object.getVariant() == 4 ? imp2016_model : classic_model;
 	}
 
 	@Override
 	public ResourceLocation getTextureResource(ImpEntity object) {
 		return object.getVariant() == 2 ? nightmareimp_texture
-				: object.getVariant() == 3 ? imp2016_texture : classic_texture;
+				:object.getVariant() == 3 ? d64_texture
+						: object.getVariant() == 4 ? imp2016_texture : classic_texture;
 	}
 
 	@Override
 	public ResourceLocation getAnimationResource(ImpEntity object) {
-		return object.getVariant() == 3 ? imp2016_animation : imp_animation;
+		return object.getVariant() == 4 ? imp2016_animation : imp_animation;
 	}
 
 	@Override
