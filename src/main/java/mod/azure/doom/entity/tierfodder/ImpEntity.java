9124e52dd217de3300505a3bb888be98d844ac1f
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-06 17:11:36 -0500

    Doom 64 Imp Variant added/Nightmare Imp model tweaked

--- src/main/java/mod/azure/doom/entity/tierfodder/ImpEntity.java
+++ src/main/java/mod/azure/doom/entity/tierfodder/ImpEntity.java
@@ -190,7 +190,7 @@ public class ImpEntity extends DemonEntity implements IAnimatable, IAnimationTic
 	}
 
 	public int getVariant() {
-		return MathHelper.clamp((Integer) this.dataTracker.get(VARIANT), 1, 3);
+		return MathHelper.clamp((Integer) this.dataTracker.get(VARIANT), 1, 4);
 	}
 
 	public void setVariant(int variant) {
@@ -198,7 +198,7 @@ public class ImpEntity extends DemonEntity implements IAnimatable, IAnimationTic
 	}
 
 	public int getVariants() {
-		return 3;
+		return 4;
 	}
 
 	@Override
@@ -206,7 +206,7 @@ public class ImpEntity extends DemonEntity implements IAnimatable, IAnimationTic
 			SpawnReason spawnReason, EntityData entityData, NbtCompound entityTag) {
 		entityData = super.initialize(serverWorldAccess, difficulty, spawnReason, entityData, entityTag);
 		SplittableRandom random = new SplittableRandom();
-		int var = random.nextInt(0, 4);
+		int var = random.nextInt(0, 5);
 		this.setVariant(var);
 		return entityData;
 	}
