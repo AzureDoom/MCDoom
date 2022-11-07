10b276b2860c18763d57b940ae6a43969139ea6c
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-06 19:23:19 -0500

    Doom 64 Pinky Variant added

--- src/main/java/mod/azure/doom/client/models/PinkyModel.java
+++ src/main/java/mod/azure/doom/client/models/PinkyModel.java
@@ -1,9 +1,10 @@
 package mod.azure.doom.client.models;
 
+import com.mojang.math.Vector3f;
+
 import mod.azure.doom.DoomMod;
 import mod.azure.doom.entity.tierheavy.PinkyEntity;
 import net.minecraft.resources.ResourceLocation;
-import com.mojang.math.Vector3f;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedTickingGeoModel;
@@ -13,13 +14,15 @@ public class PinkyModel extends AnimatedTickingGeoModel<PinkyEntity> {
 
 	@Override
 	public ResourceLocation getModelLocation(PinkyEntity object) {
-		return new ResourceLocation(DoomMod.MODID, "geo/" + (object.getVariant() == 3 ? "pinky2016" : "pinky") + ".geo.json");
+		return new ResourceLocation(DoomMod.MODID,
+				"geo/" + (object.getVariant() == 3 ? "pinky2016" : "pinky") + ".geo.json");
 	}
 
 	@Override
 	public ResourceLocation getTextureLocation(PinkyEntity object) {
-		return new ResourceLocation(DoomMod.MODID, "textures/entity/" + (object.getVariant() == 1 ? "pinky-texturemap"
-				: object.getVariant() == 3 ? "pinky2016" : "pinky_green") + ".png");
+		return new ResourceLocation(DoomMod.MODID, "textures/entity/" + (object.getVariant() == 4 ? "pinky_green"
+				: object.getVariant() == 2 ? "pinky-64" : object.getVariant() == 3 ? "pinky2016" : "pinky-texturemap")
+				+ ".png");
 	}
 
 	@Override
@@ -36,11 +39,9 @@ public class PinkyModel extends AnimatedTickingGeoModel<PinkyEntity> {
 		EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
 		if (head != null) {
 			head.setRotationX(Vector3f.XP
-					.rotation(
-							(extraData.headPitch + (entity.getVariant() == 3 ? 180 : 30)) * ((float) Math.PI / 360F))
+					.rotation((extraData.headPitch + (entity.getVariant() == 3 ? 180 : 30)) * ((float) Math.PI / 360F))
 					.i());
-			head.setRotationY(
-					Vector3f.YP.rotation(extraData.netHeadYaw * ((float) Math.PI / 500F)).j());
+			head.setRotationY(Vector3f.YP.rotation(extraData.netHeadYaw * ((float) Math.PI / 500F)).j());
 		}
 	}
 }
