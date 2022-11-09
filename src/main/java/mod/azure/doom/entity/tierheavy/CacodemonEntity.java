b5c3d3e7e399c6efac145ebebad5ab80423683fa
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-09 00:50:57 -0500

    Doom 2016 Cacodemon Variant added

--- src/main/java/mod/azure/doom/entity/tierheavy/CacodemonEntity.java
+++ src/main/java/mod/azure/doom/entity/tierheavy/CacodemonEntity.java
@@ -189,7 +189,7 @@ public class CacodemonEntity extends DemonEntity implements Monster, IAnimatable
 	}
 
 	public int getVariant() {
-		return MathHelper.clamp((Integer) this.dataTracker.get(VARIANT), 1, 3);
+		return MathHelper.clamp((Integer) this.dataTracker.get(VARIANT), 1, 4);
 	}
 
 	public void setVariant(int variant) {
@@ -197,7 +197,7 @@ public class CacodemonEntity extends DemonEntity implements Monster, IAnimatable
 	}
 
 	public int getVariants() {
-		return 3;
+		return 4;
 	}
 
 	@Override
