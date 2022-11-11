7bb4e5dc5098056b34e16d872c1ab97ea5b7ae69
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-11-11 09:03:40 -0500

    Doom 2016 Possessed Engineer Variant added

--- src/main/java/mod/azure/doom/client/models/CueballModel.java
+++ src/main/java/mod/azure/doom/client/models/CueballModel.java
@@ -1,8 +1,13 @@
 package mod.azure.doom.client.models;
 
+import com.mojang.math.Vector3f;
+
 import mod.azure.doom.DoomMod;
 import mod.azure.doom.entity.tierambient.CueBallEntity;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedTickingGeoModel;
 
 public class CueballModel extends AnimatedTickingGeoModel<CueBallEntity> {
@@ -10,22 +15,56 @@ public class CueballModel extends AnimatedTickingGeoModel<CueBallEntity> {
 	private static final ResourceLocation[] TEX = { new ResourceLocation(DoomMod.MODID, "textures/entity/cueball.png"),
 			new ResourceLocation(DoomMod.MODID, "textures/entity/cueball_flame_1.png") };
 
+	private static final ResourceLocation[] TEX2 = {
+			new ResourceLocation(DoomMod.MODID, "textures/entity/possessedengineer.png"),
+			new ResourceLocation(DoomMod.MODID, "textures/entity/possessedengineer_1.png") };
+
 	@Override
 	public ResourceLocation getModelLocation(CueBallEntity object) {
-		return new ResourceLocation(DoomMod.MODID,
-				"geo/" + (object.getVariant() == 2 ? "screecher" : "cueball") + ".geo.json");
+		return new ResourceLocation(DoomMod.MODID, "geo/"
+				+ (object.getVariant() == 3 ? "screecher" : object.getVariant() == 2 ? "possessedengineer" : "cueball")
+				+ ".geo.json");
 	}
 
 	@Override
 	public ResourceLocation getTextureLocation(CueBallEntity object) {
-		return object.getVariant() == 2 ? new ResourceLocation(DoomMod.MODID, "textures/entity/screecher.png")
-				: TEX[(object.getFlameTimer())];
+		return object.getVariant() == 3 ? new ResourceLocation(DoomMod.MODID, "textures/entity/screecher.png")
+				: object.getVariant() == 2 ? TEX2[(object.getFlameTimer())] : TEX[(object.getFlameTimer())];
 	}
 
 	@Override
 	public ResourceLocation getAnimationFileLocation(CueBallEntity object) {
 		return new ResourceLocation(DoomMod.MODID,
-				"animations/" + (object.getVariant() == 2 ? "screecher" : "rocket") + ".animation.json");
+				"animations/" + (object.getVariant() == 3 ? "screecher" : "cueengineer") + ".animation.json");
+	}
+
+	@Override
+	public void setLivingAnimations(CueBallEntity animatable, Integer instanceId, AnimationEvent animationEvent) {
+		super.setLivingAnimations(animatable, instanceId, animationEvent);
+
+		IBone Left_arm = this.getAnimationProcessor().getBone("field_191223_g");
+		IBone Right_arm = this.getAnimationProcessor().getBone("field_191224_h");
+		IBone Left_leg = this.getAnimationProcessor().getBone("field_217143_g");
+		IBone Right_leg = this.getAnimationProcessor().getBone("field_217144_h");
+
+		if (Left_arm != null && animatable.getVariant() != 3) {
+			Left_arm.setRotationX(Vector3f.XP
+					.rotation(Mth.cos(animatable.animationPosition * 0.6662F) * 2.0F * animatable.animationSpeed * 0.5F)
+					.i());
+		}
+		if (Right_arm != null && animatable.getVariant() != 3) {
+			Right_arm.setRotationX(Vector3f.XP.rotation(Mth.cos(animatable.animationPosition * 0.6662F + 3.1415927F)
+					* 2.0F * animatable.animationSpeed * 0.5F).i());
+		}
+		if (Left_leg != null && animatable.getVariant() != 3) {
+			Left_leg.setRotationX(Vector3f.XP.rotation(Mth.cos(animatable.animationPosition * 0.6662F + 3.1415927F)
+					* 1.4F * animatable.animationSpeed * 0.5F).i());
+		}
+		if (Right_leg != null && animatable.getVariant() != 3) {
+			Right_leg.setRotationX(Vector3f.XP
+					.rotation(Mth.cos(animatable.animationPosition * 0.6662F) * 1.4F * animatable.animationSpeed * 0.5F)
+					.i());
+		}
 	}
 
 }
