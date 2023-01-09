e88867f90cfb508a5c75b4a98844e7b748ac8529
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-01-08 20:26:01 -0500

    Fixes all Armor with Better Combat.

--- src/main/java/mod/azure/doom/client/render/armors/MaykrRender.java
+++ src/main/java/mod/azure/doom/client/render/armors/MaykrRender.java
@@ -2,10 +2,31 @@ package mod.azure.doom.client.render.armors;
 
 import mod.azure.doom.client.models.armor.MaykrModel;
 import mod.azure.doom.item.armor.MaykrDoomArmor;
+import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
 public class MaykrRender extends GeoArmorRenderer<MaykrDoomArmor> {
 	public MaykrRender() {
 		super(new MaykrModel());
+	}
+	
+	@Override
+	public GeoBone getLeftBootBone() {
+		return this.model.getBone("armorRightBoot").orElse(null);
+	}
+	
+	@Override
+	public GeoBone getLeftLegBone() {
+		return this.model.getBone("armorRightLeg").orElse(null);
+	}
+	
+	@Override
+	public GeoBone getRightBootBone() {
+		return this.model.getBone("armorLeftBoot").orElse(null);
+	}
+	
+	@Override
+	public GeoBone getRightLegBone() {
+		return this.model.getBone("armorLeftLeg").orElse(null);
 	}
 }
--- src/main/java/mod/azure/doom/client/render/armors/Mullet1Render.java
+++ src/main/java/mod/azure/doom/client/render/armors/Mullet1Render.java
@@ -2,10 +2,31 @@ package mod.azure.doom.client.render.armors;
 
 import mod.azure.doom.client.models.armor.Mullet1Model;
 import mod.azure.doom.item.armor.MulletDoomArmor;
+import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
 public class Mullet1Render extends GeoArmorRenderer<MulletDoomArmor> {
 	public Mullet1Render() {
 		super(new Mullet1Model());
+	}
+	
+	@Override
+	public GeoBone getLeftBootBone() {
+		return this.model.getBone("armorRightBoot").orElse(null);
+	}
+	
+	@Override
+	public GeoBone getLeftLegBone() {
+		return this.model.getBone("armorRightLeg").orElse(null);
+	}
+	
+	@Override
+	public GeoBone getRightBootBone() {
+		return this.model.getBone("armorLeftBoot").orElse(null);
+	}
+	
+	@Override
+	public GeoBone getRightLegBone() {
+		return this.model.getBone("armorLeftLeg").orElse(null);
 	}
 }
