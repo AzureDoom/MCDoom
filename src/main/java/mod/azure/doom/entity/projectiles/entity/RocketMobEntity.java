274f884b600dd5a311920a79e2fa6b505ddf254c
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-01-16 17:27:56 -0500

    Closes #83

--- src/main/java/mod/azure/doom/entity/projectiles/entity/RocketMobEntity.java
+++ src/main/java/mod/azure/doom/entity/projectiles/entity/RocketMobEntity.java
@@ -11,7 +11,6 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
@@ -136,7 +135,7 @@ public class RocketMobEntity extends AbstractHurtingProjectile implements GeoEnt
 	protected void explode() {
 		this.level.getEntities(this, new AABB(this.blockPosition().above()).inflate(4)).forEach(e -> {
 			if (e instanceof LivingEntity) {
-				e.hurt(DamageSource.playerAttack((Player) this.shooter), directHitDamage);
+				e.hurt(DamageSource.sonicBoom(this), directHitDamage);
 			}
 			this.level.explode(this, this.getX(), this.getY(0.0625D), this.getZ(), 0.0F,
 					Level.ExplosionInteraction.NONE);
