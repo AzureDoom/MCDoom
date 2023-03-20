a7b3ac257a63896d5f469d05976e5f67d07098cb
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-03-20 18:13:03 -0400

    Update ArgentEnergyItem.java

--- src/main/java/mod/azure/doom/item/ArgentEnergyItem.java
+++ src/main/java/mod/azure/doom/item/ArgentEnergyItem.java
@@ -2,11 +2,14 @@ package mod.azure.doom.item;
 
 import java.util.List;
 
+import org.jetbrains.annotations.Nullable;
+
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 
 public class ArgentEnergyItem extends Item {
@@ -34,5 +37,10 @@ public class ArgentEnergyItem extends Item {
 	@Override
 	public boolean isFireResistant() {
 		return true;
+	}
+	
+	@Override
+	public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
+		return 32767;
 	}
 }
