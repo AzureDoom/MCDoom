4544c65d3d070f12193e6bf49b94d0ee2ef0912c
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-12-07 10:49:38 -0500

    Fixes death crash

--- fabric/src/main/java/mod/azure/doom/mixins/LivingEntityMixin.java
+++ fabric/src/main/java/mod/azure/doom/mixins/LivingEntityMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,19 +23,19 @@ public class LivingEntityMixin {
     @Inject(method = "checkTotemDeathProtection", at = @At(value = "HEAD"), cancellable = true)
     private void tryUseTotem(DamageSource source, CallbackInfoReturnable<Boolean> ci) {
         final var livingEntity = (LivingEntity) (Object) this;
-        if (MCDoom.config.enable_soulcube_effects && livingEntity instanceof Player player) {
-            final var stack = TrinketsApi.getTrinketComponent(player).map(component -> {
-                final List<Tuple<SlotReference, ItemStack>> res = component.getEquipped(FabricDoomItems.SOULCUBE);
-                return res.isEmpty() ? res.get(0).getB() : ItemStack.EMPTY;
+        if (MCDoom.config.enable_soulcube_effects) {
+            var stack = TrinketsApi.getTrinketComponent(livingEntity).map(component -> {
+                List<Tuple<SlotReference, ItemStack>> res = component.getEquipped(FabricDoomItems.SOULCUBE);
+                return res.size() > 0 ? res.get(0).getB() : ItemStack.EMPTY;
             }).orElse(ItemStack.EMPTY);
 
             if (!stack.isEmpty()) {
-                stack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(player.getUsedItemHand()));
-                player.setHealth(20.0F);
-                player.removeAllEffects();
-                player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 100, 4));
-                player.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 100, 4));
-                player.level().broadcastEntityEvent(player, (byte) 95);
+                stack.hurtAndBreak(1, livingEntity, p -> p.broadcastBreakEvent(livingEntity.getUsedItemHand()));
+                livingEntity.setHealth(20.0F);
+                livingEntity.removeAllEffects();
+                livingEntity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 100, 4));
+                livingEntity.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 100, 4));
+                livingEntity.level().broadcastEntityEvent(livingEntity, (byte) 95);
                 ci.setReturnValue(true);
             }
         }
