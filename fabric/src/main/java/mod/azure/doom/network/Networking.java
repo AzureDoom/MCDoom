fcc4f3a742b48be8f5fff966b418e5c4d76e42ca
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2024-03-26 17:48:15 -0400

    Network tweaking for crashing

--- fabric/src/main/java/mod/azure/doom/network/Networking.java
+++ fabric/src/main/java/mod/azure/doom/network/Networking.java
@@ -10,26 +10,36 @@ public final class Networking {
         ServerPlayNetworking.registerGlobalReceiver(DoomNetwork.LOCK_SLOT, new C2SMessageSelectCraft());
         ServerPlayNetworking.registerGlobalReceiver(DoomNetwork.RELOAD,
                 (server, player, serverPlayNetworkHandler, inputPacket, packetSender) -> {
-                    if (!player.isCreative() && player.getMainHandItem().getItem() instanceof DoomBaseItem)
-                        DoomBaseItem.reload(player, player.getUsedItemHand());
+                    server.execute(() -> {
+                        if (!player.isCreative() && player.getMainHandItem().getItem() instanceof DoomBaseItem)
+                            DoomBaseItem.reload(player, player.getUsedItemHand());
+                    });
                 });
         ServerPlayNetworking.registerGlobalReceiver(DoomNetwork.SHOOT,
                 (server, player, serverPlayNetworkHandler, inputPacket, packetSender) -> {
-                    if (player.getMainHandItem().getItem() instanceof DoomBaseItem) DoomBaseItem.shoot(player);
+                    server.execute(() -> {
+                        if (player.getMainHandItem().getItem() instanceof DoomBaseItem) DoomBaseItem.shoot(player);
+                    });
                 });
         ServerPlayNetworking.registerGlobalReceiver(DoomNetwork.HOOK,
                 (server, player, serverPlayNetworkHandler, inputPacket, packetSender) -> {
-                    if (player.getMainHandItem().getItem() instanceof DoomBaseItem) DoomBaseItem.shootHook(player);
+                    server.execute(() -> {
+                        if (player.getMainHandItem().getItem() instanceof DoomBaseItem) DoomBaseItem.shootHook(player);
+                    });
                 });
         ServerPlayNetworking.registerGlobalReceiver(DoomNetwork.RELOAD_MELEE,
                 (server, player, serverPlayNetworkHandler, inputPacket, packetSender) -> {
-                    if (!player.isCreative() && player.getMainHandItem().getItem() instanceof BaseSwordItem)
-                        BaseSwordItem.reload(player, player.getUsedItemHand());
+                    server.execute(() -> {
+                        if (!player.isCreative() && player.getMainHandItem().getItem() instanceof BaseSwordItem)
+                            BaseSwordItem.reload(player, player.getUsedItemHand());
+                    });
                 });
         ServerPlayNetworking.registerGlobalReceiver(DoomNetwork.FIREMODE,
                 (server, player, serverPlayNetworkHandler, inputPacket, packetSender) -> {
-                    if (player.getMainHandItem().getItem() instanceof DoomBaseItem)
-                        DoomBaseItem.changeFireMode(player.getMainHandItem(), player);
+                    server.execute(() -> {
+                        if (player.getMainHandItem().getItem() instanceof DoomBaseItem)
+                            DoomBaseItem.changeFireMode(player.getMainHandItem(), player);
+                    });
                 });
     }
 }
